573c85c7e21ae1032846e69d155800c45c1db66d
mxwllndrsn <mxwllndrsn@gmail.com>
2019-01-31 11:16:25 -0800

    final button fix

--- src/pages/fullpage-test.js
+++ src/pages/fullpage-test.js
@@ -173,13 +173,14 @@ const FullPage  = ({data}) => {
                     }}>
                     <h2 style={{textAlign:"right"}}>SERVICE</h2>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br />
                     <button style={{
                         position: "relative",
                         top: "3vh",
                         backgroundColor: "#FFFFFF",
                         border: "none",
                         color: "black",
-                        display: "block",
+                        display: "inline-block",
                         padding: ".5em 1em",
                         fontFamily: "Helvetica, sans-serif",
                         fontSize: "2vh",
