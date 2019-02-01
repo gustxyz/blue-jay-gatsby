8e225c726f670e04319b4fc437992eb1bc706b42
Alexander Long <alexanderlcodes@gmail.com>
2019-01-31 19:40:29 -0800

    Example css

--- src/pages/fullpage-test.js
+++ src/pages/fullpage-test.js
@@ -14,8 +14,8 @@ const minerGold = {
 
 const FullPage  = ({data}) => {
   //const pages = data.allMarkdownRemark.edges;
-
   //console.log(pages);
+
       return (
 
         <Layout>
@@ -34,15 +34,18 @@ const FullPage  = ({data}) => {
               backgroundImage: "url(http://thebluejaygroup.com/gatsby/img/hero/Site_Hero_1_BLUEJAY.png)"
             }}
           className="section">
-              <div className="heroHeadingLeft">
-                  BLUE JAY <br />
-                  SCREEN PRINTING
-                    <div className="heroCopyLeft">
-                      <h2>QUALITY</h2>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                      <br />
-                      <button className="btn"> <span style={brandBlue}>LEARN MORE</span> </button>
-                    </div>
+              <div className="hero-heading margin-left">
+                  <h1>
+                    BLUE JAY 
+                    <br/>
+                    SCREEN PRINTING
+                  </h1>
+                  <div className="hero-passage">
+                    <h2>QUALITY</h2>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br />
+                    <button className="hero-btn"> <span style={brandBlue}>LEARN MORE</span> </button>
+                  </div>
               </div>
           </div>
           <div
